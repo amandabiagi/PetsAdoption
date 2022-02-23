d103dd1583e30234dc78e7a62a4e80670d1a295a
Amanda Biagi <amanda.biagi@iatec.com.br>
2022-02-23 19:37:45 -0300

    Adding rule business in domain Pet

--- API/PetsAdoption/PetsAdoption.Domain/Entities/Pet.cs
+++ API/PetsAdoption/PetsAdoption.Domain/Entities/Pet.cs
@@ -1,4 +1,5 @@
 ﻿using PetsAdoption.Domain.Entities.Enum;
+using PetsAdoption.Domain.Entities.Validation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,88 @@ namespace PetsAdoption.Domain.Entities
         public PetSexEnum Sex { get; set; }
         public PetSizeEnum Size { get; set; }
         public string Breed { get; set; }
+        public int InstituteId { get; set; }
         public Institute Institute { get; set; }
 
 
-        private void ValidateDomain(string name,PetTypeEnum type, int year, int months, PetSexEnum sex, PetSexEnum size, string breed, Institute institute)
+        public Pet(
+            string name,
+            PetTypeEnum type,
+            int year,
+            int months,
+            PetSexEnum sex,
+            PetSexEnum size,
+            string breed)
         {
-            
+
+            ValidationPets(name, type, year, months, sex, size, breed);
+        }
+
+        public Pet(
+          int id,
+          string name,
+          PetTypeEnum type,
+          int year,
+          int months,
+          PetSexEnum sex,
+          PetSexEnum size,
+          string breed)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid ID value. ");
+            ValidationPets(name, type, year, months, sex, size, breed);
+        }
+
+        public void Update(
+         string name,
+         PetTypeEnum type,
+         int year,
+         int months,
+         PetSexEnum sex,
+         PetSexEnum size,
+         string breed,
+         int instituteId)
+        {
+
+            ValidationPets(name, type, year, months, sex, size, breed);
+            InstituteId = instituteId;
+        }
+
+        private void ValidationPets(
+            string name,
+            PetTypeEnum type,
+            int year,
+            int months,
+            PetSexEnum sex,
+            PetSexEnum size,
+            string breed
+            )
+        {
+            DomainExceptionValidation.When(name.Length <= 1,
+                                            "Invalid name, too short, minimum 2 caracteres");
+            DomainExceptionValidation.When(name.Length > 10,
+                                            "Invalid name, too long, maximum 10 caracteres");
+            DomainExceptionValidation.When(type == null, 
+                                            "Invalid type, Type is required");
+            DomainExceptionValidation.When(year < 0,
+                                            "Invalid year value");
+            DomainExceptionValidation.When(months < 0,
+                                            "Invalid monthes value");
+            DomainExceptionValidation.When(sex == null, 
+                                            "Invalid sex, Sex is required");
+            DomainExceptionValidation.When(size == null,        
+                                            "Invalid size, Size is required");
+            DomainExceptionValidation.When(breed.Length > 25, 
+                                            "Invalid breed, too long, maximum 25 caracteres");
+
+            Name = name;
+            Type = type;
+            Year = year;
+            Months = months;
+            Sex = sex;
+            Size = (PetSizeEnum)size;
+            Breed = breed;
+
+
         }
 
     }
---
+++ API/PetsAdoption/PetsAdoption.Domain/Entities/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsAdoption.Domain.Entities.Validation 
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string message) : base(message)
+        {
+
+        }
+
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(message);
+            }
+        }
+
+    }
+}
